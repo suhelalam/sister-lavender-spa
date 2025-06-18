9f2927ba14071ef5a8355a3db5744e5f66fe5854
suhelalam <suhelalam415@gmail.com>
2025-06-18 12:45:22 -0500

    updated

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -14,11 +14,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/index.js"
     ],
-    "/AllServices": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/AllServices.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -28,6 +23,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/services": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/services.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6925877b41f3509"; }
+/******/ 		__webpack_require__.h = function() { return "f6f1cfeedd02b4fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
